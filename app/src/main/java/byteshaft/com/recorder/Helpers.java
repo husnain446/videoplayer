3380364acb852de40ac6936b0553d98976a2d94d
bilal shahid <s9iper1@gmail.com>
2015-04-14 16:32:20 +0500

    Change UI

--- app/src/main/java/byteshaft/com/recorder/Helpers.java
+++ app/src/main/java/byteshaft/com/recorder/Helpers.java
@@ -3,6 +3,8 @@ package byteshaft.com.recorder;
 
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -77,6 +79,8 @@ public class Helpers extends ContextWrapper {
         }
         mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
         mp.setDisplay(holder);
+        mp.attachAuxEffect(0);
+        mp.getDuration();
         try {
             mp.setDataSource(getApplicationContext(), videoUri);
             mp.prepare();
@@ -94,6 +98,9 @@ public class Helpers extends ContextWrapper {
         ArrayAdapter<String> modeAdapter = new ArrayAdapter<>(
                 getApplicationContext(), android.R.layout.simple_list_item_1, realVideos);
         list.setAdapter(modeAdapter);
+        ColorDrawable white = new ColorDrawable(this.getResources().getColor(R.color.sage));
+        list.setDivider(white);
+        list.setDividerHeight(1);
         return list;
     }
 
