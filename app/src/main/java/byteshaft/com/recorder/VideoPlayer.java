1a775ebb3d380bce6659b5e9bf5dbd7788f4da1c
Omer Akram <om26er@gmail.com>
2015-04-15 12:36:20 +0500

    use VideoView to ease the logic

--- app/src/main/java/byteshaft/com/recorder/VideoPlayer.java
+++ app/src/main/java/byteshaft/com/recorder/VideoPlayer.java
@@ -6,21 +6,41 @@ import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.VideoView;
 
 
-public class VideoPlayer extends Activity implements SurfaceHolder.Callback,
-        MediaPlayer.OnCompletionListener, SurfaceView.OnLongClickListener,
-        MediaPlayer.OnPreparedListener, SurfaceView.OnClickListener {
+public class VideoPlayer extends Activity implements MediaPlayer.OnCompletionListener,
+         VideoView.OnTouchListener, Button.OnClickListener {
 
-    private Uri uri = null;
-    private MediaPlayer mMediaPlayer = null;
     private VideoOverlay mVideoOverlay = null;
     private String videoPath = null;
-    private Helpers mHelpers = null;
+    private VideoView videoView = null;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (videoView.isPlaying()) {
+            videoView.pause();
+        } else {
+            videoView.start();
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        videoView.pause();
+        mVideoOverlay.setVideoFile(videoPath);
+        mVideoOverlay.setVideoStartPosition(videoView.getCurrentPosition());
+        mVideoOverlay.startPlayback();
+        finish();
+        showDesktop();
+    }
 
     private static class Screen {
         static class Brightness {
@@ -33,68 +53,29 @@ public class VideoPlayer extends Activity implements SurfaceHolder.Callback,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         Bundle bundle = getIntent().getExtras();
         videoPath = bundle.getString("videoUri");
-        uri = Uri.parse(videoPath);
-        mMediaPlayer = new MediaPlayer();
-        mMediaPlayer.setOnCompletionListener(this);
-        mMediaPlayer.setOnPreparedListener(this);
-        SurfaceView mSurfaceView = (SurfaceView) findViewById(R.id.display);
-        mSurfaceView.setOnLongClickListener(this);
-        mSurfaceView.setOnClickListener(this);
-        SurfaceHolder surfaceHolder = mSurfaceView.getHolder();
-        surfaceHolder.addCallback(this);
+        videoView = (VideoView) findViewById(R.id.videoSurface);
+        videoView.setOnCompletionListener(this);
+        videoView.setOnTouchListener(this);
+        Button button = (Button) findViewById(R.id.overlayButton);
+        button.setOnClickListener(this);
         mVideoOverlay = new VideoOverlay(getApplicationContext());
         setScreenBrightness(Screen.Brightness.HIGH);
+        videoView.setVideoPath(videoPath);
+        videoView.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mMediaPlayer.release();
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        mHelpers = new Helpers(getApplicationContext());
-        mHelpers.prepareMediaPlayer(mMediaPlayer, uri, holder);
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
         setScreenBrightness(Screen.Brightness.DEFAULT);
-    }
-
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-        mp.start();
+        videoView.stopPlayback();
     }
 
     @Override
     public void onCompletion(MediaPlayer mp) {
         finish();
-    }
-
-    @Override
-    public boolean onLongClick(View v) {
-        mMediaPlayer.pause();
-        mVideoOverlay.setVideoFile(videoPath);
-        mVideoOverlay.setVideoStartPosition(mMediaPlayer.getCurrentPosition());
-        mVideoOverlay.startPlayback();
-        finish();
-        showDesktop();
-        return false;
-    }
-
-    @Override
-    public void onClick(View v) {
-        mHelpers.togglePlayback(mMediaPlayer);
     }
 
     private void setScreenBrightness(float value) {
