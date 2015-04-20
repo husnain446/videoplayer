4c0106d268470b917fece9cf126d7808af861b77
Omer Akram <om26er@gmail.com>
2015-04-20 12:36:17 +0500

    Make thumbnail loading async, almost fix touch drag events on screen

--- app/src/main/java/byteshaft/com/recorder/VideoPlayer.java
+++ app/src/main/java/byteshaft/com/recorder/VideoPlayer.java
@@ -3,6 +3,7 @@ package byteshaft.com.recorder;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -15,17 +16,25 @@ import android.widget.VideoView;
 public class VideoPlayer extends Activity implements MediaPlayer.OnCompletionListener,
         Button.OnClickListener, View.OnTouchListener {
 
-    private VideoOverlay mVideoOverlay = null;
     private String videoPath = null;
     private VideoView videoView = null;
-    private float preValue = 0;
     private boolean clicked = false;
     private static boolean isLandscape = true;
+    private float relevantMoveStep = 0;
+    private float initialTouchY = 0;
 
     private static class Screen {
         static class Brightness {
             static final float HIGH = 1f;
             static final float DEFAULT = -1f;
+            static final float LOW = 0f;
+        }
+    }
+
+    private static class Sound {
+        static class Level {
+            static final int MINIMUM = 0;
+            static final int MAXIMUM = 15;
         }
     }
 
@@ -38,7 +47,6 @@ public class VideoPlayer extends Activity implements MediaPlayer.OnCompletionLis
         videoView = (VideoView) findViewById(R.id.videoSurface);
         videoView.setOnCompletionListener(this);
         videoView.setOnTouchListener(this);
-        mVideoOverlay = new VideoOverlay(getApplicationContext());
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setScreenBrightness(Screen.Brightness.HIGH);
         android.widget.MediaController controller = new android.widget.MediaController(this);
@@ -57,31 +65,67 @@ public class VideoPlayer extends Activity implements MediaPlayer.OnCompletionLis
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        float brightness = getCurrentBrightness();
-        if (event.getX() < v.getWidth() / 2 && event.getY() > 0) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    preValue = event.getY();
-                    clicked = true;
-                    return true;
-                case MotionEvent.ACTION_MOVE:
-                    if (event.getRawY() > preValue) {
+        final double BRIGHTNESS_STEP = 0.075;
+        final int VOLUME_STEP = 1;
+        final int ACTIVITY_HEIGHT_FRAGMENT = v.getHeight() / 15;
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                initialTouchY = event.getY();
+                clicked = true;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                float touchX = event.getX();
+                float touchY = event.getY();
+                // If action is on the left half of the View.
+                if (touchX < v.getWidth() / 2) {
+                    float brightness = getCurrentBrightness();
+                    if (touchY > initialTouchY &&
+                            brightness - BRIGHTNESS_STEP > Screen.Brightness.LOW) {
                         System.out.println("Going DOWN");
-                        brightness-=0.010;
-                        setScreenBrightness(brightness);
-                    } else {
+                        relevantMoveStep = initialTouchY + ACTIVITY_HEIGHT_FRAGMENT;
+                        if (touchY >= relevantMoveStep) {
+                            brightness-= BRIGHTNESS_STEP;
+                            setScreenBrightness(brightness);
+                            relevantMoveStep += ACTIVITY_HEIGHT_FRAGMENT;
+                        }
+                    } else if (touchY < initialTouchY &&
+                            brightness + BRIGHTNESS_STEP <= Screen.Brightness.HIGH) {
                         System.out.println("Going UP");
-                        brightness+=0.005;
-                        setScreenBrightness(brightness);
+                        relevantMoveStep = initialTouchY - ACTIVITY_HEIGHT_FRAGMENT;
+                        if (touchY <= relevantMoveStep) {
+                            brightness += BRIGHTNESS_STEP;
+                            setScreenBrightness(brightness);
+                            relevantMoveStep -= ACTIVITY_HEIGHT_FRAGMENT;
+                        }
                     }
-                    clicked = false;
-                    return true;
-                case MotionEvent.ACTION_UP:
-                    if (clicked) {
-                        togglePlayback();
+                } else {
+                    int volume = getCurrentVolume();
+                    if (touchY > initialTouchY &&
+                            volume - VOLUME_STEP >= Sound.Level.MINIMUM) {
+                        relevantMoveStep = initialTouchY + ACTIVITY_HEIGHT_FRAGMENT;
+                        if (touchY >= relevantMoveStep) {
+                            volume -= VOLUME_STEP;
+                            setVolume(volume);
+                            relevantMoveStep += ACTIVITY_HEIGHT_FRAGMENT;
+                        }
+                    } else if (touchY < initialTouchY &&
+                            volume + VOLUME_STEP <= Sound.Level.MAXIMUM) {
+                        relevantMoveStep = initialTouchY - ACTIVITY_HEIGHT_FRAGMENT;
+                        if (touchY <= relevantMoveStep) {
+                            volume += VOLUME_STEP;
+                            setVolume(volume);
+                            relevantMoveStep -= ACTIVITY_HEIGHT_FRAGMENT;
+                        }
                     }
-                    return true;
-            }
+                }
+                clicked = false;
+                return true;
+            case MotionEvent.ACTION_UP:
+                if (clicked) {
+                    togglePlayback();
+                }
+                return true;
         }
         return false;
     }
@@ -91,20 +135,20 @@ public class VideoPlayer extends Activity implements MediaPlayer.OnCompletionLis
         switch (v.getId()) {
             case R.id.overlayButton:
                 videoView.pause();
-                mVideoOverlay.setVideoFile(videoPath);
-                mVideoOverlay.setVideoStartPosition(videoView.getCurrentPosition());
-                mVideoOverlay.startPlayback();
+                VideoOverlay videoOverlay = new VideoOverlay(getApplicationContext());
+                videoOverlay.setVideoFile(videoPath);
+                videoOverlay.setVideoStartPosition(videoView.getCurrentPosition());
+                videoOverlay.startPlayback();
                 finish();
                 showDesktop();
                 break;
             case R.id.bRotate:
                 if (isLandscape) {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-                    isLandscape = !isLandscape;
                 } else {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                    isLandscape = !isLandscape;
                 }
+                isLandscape = !isLandscape;
         }
     }
 
@@ -115,9 +159,6 @@ public class VideoPlayer extends Activity implements MediaPlayer.OnCompletionLis
 
     private void setScreenBrightness(float value) {
         System.out.println(String.format("Attempted value %f", value));
-        if (value <= 0.010 || value > 1) {
-            return;
-        }
         WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
         layoutParams.screenBrightness = value;
         getWindow().setAttributes(layoutParams);
@@ -140,5 +181,15 @@ public class VideoPlayer extends Activity implements MediaPlayer.OnCompletionLis
         } else {
             videoView.start();
         }
+    }
+
+    private int getCurrentVolume() {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        return am.getStreamVolume(AudioManager.STREAM_MUSIC);
+    }
+
+    private void setVolume(int level) {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        am.setStreamVolume(AudioManager.STREAM_MUSIC, level, 0);
     }
 }
