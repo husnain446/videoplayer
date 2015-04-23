d69183ba8b16fc4f4ee6e0d814a543adfe1640e4
Omer Akram <om26er@gmail.com>
2015-04-23 17:55:02 +0500

    Reduce navigation drawer code to only whats needed, brought back search

--- app/src/main/java/byteshaft/com/recorder/VideoPlayer.java
+++ app/src/main/java/byteshaft/com/recorder/VideoPlayer.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.wifi.WifiManager;
 import android.widget.MediaController;
 import android.os.Bundle;
 import android.view.MotionEvent;
