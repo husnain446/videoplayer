1e76c04f13ba68d06d1755790e6c0e57d61845c6
Omer Akram <om26er@gmail.com>
2015-04-24 17:48:21 +0500

    Stop using MediaMetadataRetriever for getting video properties, inquire that through VideoView

--- app/src/main/java/byteshaft/com/recorder/CustomVideoView.java
+++ app/src/main/java/byteshaft/com/recorder/CustomVideoView.java
@@ -1,26 +1,31 @@
 package byteshaft.com.recorder;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.util.AttributeSet;
 import android.widget.VideoView;
 
 import java.util.ArrayList;
 
-public class CustomVideoView extends VideoView {
+public class CustomVideoView extends VideoView implements MediaPlayer.OnPreparedListener {
 
     static final int PLAYING = 1;
     static final int PAUSED = 0;
 
     private Uri currentlyPlayingVideoUri = null;
     private ArrayList<MediaPlayerStateChangedListener> mListeners = new ArrayList<>();
+    private int mVideoHeight = 0;
+    private int mVideoWidth = 0;
 
     public CustomVideoView(Context context) {
         super(context);
+        setOnPreparedListener(this);
     }
 
     public CustomVideoView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOnPreparedListener(this);
     }
 
     public void setMediaPlayerStateChangedListener(MediaPlayerStateChangedListener listener) {
@@ -59,6 +64,20 @@ public class CustomVideoView extends VideoView {
 
     public Uri getVideoURI() {
         return currentlyPlayingVideoUri;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mVideoHeight = mp.getVideoHeight();
+        mVideoWidth = mp.getVideoWidth();
+    }
+
+    public int getVideoHeight() {
+        return mVideoHeight;
+    }
+
+    public int getVideoWidth() {
+        return mVideoWidth;
     }
 
     public interface MediaPlayerStateChangedListener {
